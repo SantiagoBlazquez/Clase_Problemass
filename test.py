5b7f66288044f4a9c3714dfdd70e7880ed59184c
SantiagoBlazquez <santiagonblazquez@gmail.com>
2025-04-03 16:19:19 -0300

    Funcion Factorial Arreglada

--- test.py
+++ test.py
@@ -6,8 +6,8 @@ def factorial(num):
     if num == 0:
         return 1
     else:
-        return num * factorial(num)
+        return num * factorial(num - 1)
     
 numero = int(input("Ingrese un numero: "))
-print(f"El factorial de {numero} es: factorial(numero)")
+print(f"El factorial de {numero} es: {factorial(numero)}")
     
